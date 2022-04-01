1895091149494c288af315a55bdb022d3d7b9d86
Jairo Matos da Rocha <jairo@discente.ufg.br>
2022-04-01 19:50:59 -0300

    teste

--- src/grads.py
+++ src/grads.py
@@ -1,14 +1,10 @@
-from glob impor glob
+from glob import glob
 
-from cirrus.util.config import settings, logger
+#from cirrus.util.config import settings, logger
 
 
 
 from xgrads import CtlDescriptor, open_CtlDataset
 
-def grads_to_tiff():
-    downloads_dir = f'{settings.CEMPADIR}downloads'
-    files = glob(f'{downloads_dir}/*.ctl')
-    BRAMS_data = files[0]
-    data1 = open_CtlDataset(BRAMS_data)
-    data1
+if __name__ == '__main__':
+    print('import all')
