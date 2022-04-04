75777cd77461e22efe5cf21d8b5c8d2c0ce25134
Jairo Matos da Rocha <jairo@discente.ufg.br>
2022-04-04 09:23:33 -0300

    modificado cli para start

--- src/start.py
+++ src/start.py
@@ -2,7 +2,6 @@ from os import environ, mkdir
 from os.path import isdir
 from shutil import rmtree
 
-import click
 from dynaconf import Dynaconf
 
 from cirrus.model import clear_tables
@@ -15,22 +14,13 @@ initial_config = Dynaconf(
     settings_files=['settings.toml', '.secrets.toml'],
 )
 
-
-@click.command()
-@click.option('--force_save_bd/--no-force_save_bd', default=False)
-def cli_main(force_save_bd):
-    environ['CEMPA_FORCE_SAVE_BD'] = str(force_save_bd).lower()
-
-    logger.info(
-        f'Numero de pool {initial_config.N_POOL} force_save = {initial_config.FORCE_SAVE_BD}'
-    )
+def main():
+    logger.info(f'Numero de pool {initial_config.N_POOL}')
     if downloads_files():
         clear_tables()
-        
-        pass
-    
+    else:
     #main(initial_config.FORCE_SAVE_BD)
 
 
 if __name__ == '__main__':
-    cli_main()
+    main()
