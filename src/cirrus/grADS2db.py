2dd02608973c3dc73a32a5547fb3954782da85ca
Jairo Matos Da Rocha <nxgame2009@gmail.com>
2022-04-04 20:18:09 -0300

    Update grADS2db.py

--- src/cirrus/grADS2db.py
+++ src/cirrus/grADS2db.py
@@ -54,7 +54,7 @@ def grads_to_sql(file_name):
             temp_df = pd.DataFrame(
                 {'datetime': vtime, **layers, 'point_gid': gid}
             )
-            save_df_bd(temp_df, name.lower())
+            save_df_bd(temp_df, name.lower(),file_name)
             _min = temp_df.min()
             _max = temp_df.max()
             dfs[name] = pd.concat(
