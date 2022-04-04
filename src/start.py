1cbf614af9bb6d9b17f91b4810922d744cae97c4
Jairo Matos da Rocha <jairo@discente.ufg.br>
2022-04-04 18:07:57 -0300

    ativnado o model

--- src/start.py
+++ src/start.py
@@ -5,7 +5,7 @@ from os import mkdir
 from dynaconf import Dynaconf
 from cirrus.grADS2db import to_db
 
-#from cirrus.model import clear_tables
+from cirrus.model import clear_tables
 #from cirrus.netcdf2postgis import main
 from cirrus.util.config import logger, settings
 from cirrus.dowloads import downloads_files
@@ -18,7 +18,7 @@ initial_config = Dynaconf(
 def main():
     logger.info(f'Numero de pool {initial_config.N_POOL}')
     #if downloads_files():
-        #clear_tables()
+    clear_tables()
     tifs_path = f'{settings.CATALOG}cempa_tifs'
     if isdir(tifs_path):
         rmtree(tifs_path)
