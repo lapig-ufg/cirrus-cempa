fa62b3fe8b3847279dae0048bf3b94f49179d70b
Jairo Matos da Rocha <jairo@discente.ufg.br>
2022-04-04 12:31:35 -0300

    convertendo grADS db and tif

---
+++ src/cirrus/grADS2db.py
@@ -0,0 +1,79 @@
+import numpy as np
+from cirrus.grADS2tif import grADS2tiff
+import rioxarray
+from xgrads import open_CtlDataset
+import pandas as pd
+from multiprocessing import Pool
+from glob import glob
+
+
+from cirrus.config import variables, settings
+
+
+def get_time(dataframe):
+    return pd.to_datetime(dataframe.time.data[0])
+
+
+def grads_to_sql(file_name):
+    dfs = {}
+    print(file_name)
+    with open_CtlDataset(file_name) as file:
+        dataframe = file
+        for name in variables:
+            print(file_name, name)
+
+            var = dataframe.variables[name][0]
+            vtime, *_ = [
+                x.flatten()
+                for x in np.meshgrid(
+                    get_time(dataframe),
+                    dataframe.lat,
+                    dataframe.lon,
+                    indexing='ij',
+                )
+            ]
+            layers = {}
+            gid = [i for i, _ in enumerate(vtime, 1)]
+            for _id, layer_name in variables[name]['layers']:
+                if _id > -1:
+                    grADS2tiff(dataframe, name, layer_name,_id)
+                    layers[layer_name] = np.meshgrid(var[_id], indexing='ij')[
+                        0
+                    ]
+                else:
+                    grADS2tiff(dataframe, name, layer_name)
+                    layers[layer_name] = np.meshgrid(var, indexing='ij')[0]
+
+            temp_df = pd.DataFrame(
+                {'datetime': vtime, **layers, 'point_gid': gid}
+            )
+            _min = temp_df.min()
+            _max = temp_df.max()
+            dfs[name] = pd.concat(
+                [
+                    pd.DataFrame(_max).T.drop(columns='point_gid'),
+                    pd.DataFrame(_min).T.drop(columns='point_gid'),
+                ]
+            )
+    return dfs
+
+def to_db():
+    with Pool(settings.N_POO) as workers:
+        returns = workers.map(grads_to_sql, glob(f'{settings.CEMPADIR}downloads/*.ctl')[:10])
+
+
+    tmp_max_minx = {}
+    for result in returns:
+        for i in result:
+            try:
+                tmp_max_minx[i] = pd.concat([tmp_max_minx[i], result[i]])
+            except Exception as e:
+                tmp_max_minx[i] = result[i]
+
+
+    max_minx = {}
+    for name in tmp_max_minx:
+        max_minx[name] = {
+            'max': tmp_max_minx[name].resample('D', on='datetime').max(),
+            'min': tmp_max_minx[name].resample('D', on='datetime').min(),
+        }
---
+++ src/cirrus/grADS2tif.py
@@ -0,0 +1,53 @@
+from shutil import rmtree
+import numpy as np
+
+from os import mkdir
+from os.path import isdir
+
+
+def create_folder_for_tiffs(path_level1, name):
+    if not isdir(path_level1):
+        mkdir(path_level1)
+    if not isdir(f'{path_level1}/{name}'):
+        mkdir(f'{path_level1}/{name}')
+
+
+def get_time(raster):
+    dt = raster.time.data
+    return np.datetime_as_string(dt, unit='m').replace(':', '-')
+
+
+def grADS2tiff(raster, name, coll_name, id_level=None):
+    """_summary_
+
+    Args:
+        name (_type_): _description_
+        coll_name (_type_): _description_
+        file_name (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    try:
+        raster = dataframe[name][0]
+        file_date = get_time(raster)
+        tifs_path = f'{settings.CEMPADIR}tifs'
+        if isdir(tifs_path):
+            rmtree(tifs_path)
+        mkdir(tifs_path)
+
+        path_level1 = f'{tifs_path}/{file_date}'
+        create_folder_for_tiffs(path_level1, name)
+        name_tif = f'{path_level1}/{name}/{coll_name}.tif'
+        if isinstance(id_level, int):
+            raster = raster.isel(lev=id_level).rio.set_spatial_dims(
+                'lon', 'lat'
+            )
+        else:
+            raster = raster.rio.set_spatial_dims('lon', 'lat')
+
+        raster.rio.set_crs('epsg:4674')
+        raster.rio.to_raster(name_tif)
+        return True
+    except:
+        return False
