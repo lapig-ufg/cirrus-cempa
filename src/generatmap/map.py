56871573906db9eba6e638b8b02b0aafb4cbf47f
Jairo Matos da Rocha <jairo@discente.ufg.br>
2022-04-04 16:40:32 -0300

    colocando no padrao do mapserver

--- src/generatmap/map.py
+++ src/generatmap/map.py
@@ -10,7 +10,7 @@ from cirrus.util.functions import get_min_max, get_pallet
 def creat_map_file(
     file_name, name, coll_name, min_max, file_date='', geotiff=True
 ):
-    logger.info('ADD no .map para o tiff {file_name}')
+    
     env = Environment(
         loader=PackageLoader('generatmap'), autoescape=select_autoescape()
     )
@@ -18,8 +18,8 @@ def creat_map_file(
     with open(f'{settings.CATALOG}{settings.MAPFILE}', 'a') as file_object:
         template = env.get_template('cempa.map')
         row = {}
-        logger.info('Gerando .map com dados padrao')
-        title = f"{name.lower()} {coll_name.replace('_', ' ').replace('value','')} {file_date}"
+        logger.info(f'ADD no .map para o tiff {file_name}')
+        title = f"{name.lower()}_{coll_name.replace('value','')}_{file_date}"
         row['view_name'] = title
         row['ows_title'] = title
         row['ows_abstract'] = title
