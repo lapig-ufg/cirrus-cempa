938f22a31ba85cb9429297270037fe11c4c34f98
Jairo Matos da Rocha <jairo@discente.ufg.br>
2022-04-11 18:02:56 -0300

    teste email

--- src/colarazi.py
+++ src/colarazi.py
@@ -2,6 +2,7 @@ root = '/STORAGE/catalog/cempa_tifs/2022-04-06T14-00/albedt'
 
 import rasterio
 
+from cirrus.util.config import logger
 with rasterio.open(f'{root}/value.tif') as src:
         shade = src.read(1)
         meta = src.meta
@@ -12,3 +13,7 @@ with rasterio.open(f'{root}/color_value.tif', 'w', **meta) as dst:
             1, {
                 0: (255, 0, 0, 255),
                 255: (0, 0, 255, 255) })
+
+
+
+logger.error('teste')
