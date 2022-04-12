17d80a5342ded47d32ab62c0758370b4d62b683b
Jairo Matos da Rocha <jairo@discente.ufg.br>
2022-04-12 15:37:38 -0300

    status code

--- src/start.py
+++ src/start.py
@@ -43,7 +43,7 @@ def main():
         to_db()
 
         restart_ows = post(initial_config.CEMPA_OWS_URL)
-        print(restart_ows.status)
+        print(restart_ows.status_code)
         print(restart_ows.text)
         if isdir(initial_config.OWS_CACH):
             rmtree(initial_config.OWS_CACH)
